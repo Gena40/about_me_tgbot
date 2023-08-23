a1a74b36ad1673515bba1cd505e6bc61f03d680d
Gennadi <g.romanov@innopolis.ru>
2023-08-23 20:44:17 +0300

    Finish commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,3 @@ from os.path import abspath, dirname
 
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
-
-# pytest_plugins = [
-#     'tests.fixtures.fixture_data'
-# ]
-# import os
-# import pytest
-# from unittest import mock
-
-
-# @pytest.fixture
-# def mock_env(monkeypatch):
-#     from main import check_token
-#     def mock_env(*args, **kwargs):
-#         pass
-#     monkeypatch.setattr('')
